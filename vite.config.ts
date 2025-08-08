e2088d2ef7d6795e34f1584583047d426d9fccd3
mickeyLalata <mckyl1108@gmail.com>
2025-08-09 00:13:04 +0800

    edited for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/My_Portfolio/', // ← must match your GitHub repo name
   plugins: [
     react(),
     tailwindcss()
